c6e8a0a0fe23fc7375e473d29c3fe4a82cf80e89
David Bermudez <davidbermudez97@gmail.com>
2023-12-11 22:47:44 -0500

    Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from "./test/test.component"; //add IMPORT HERE to display
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, TestComponent]
+    imports: [
+      CommonModule, 
+      RouterOutlet, 
+      TestComponent,
+      FormsModule
+    ]
 })
 
 export class AppComponent {
@@ -28,21 +34,63 @@ export class AppComponent {
 // 10. Event biding with a Click Event
 display(msg:string){
   alert(msg);
-
-}
-dblClick=0;
+  }
+//11. Event Binding with Click Event
+dblClick = 0;
 incrementOnDoubleClick(){
   this.dblClick++;
-}
+  }
 
-onOver=0;
+onOver = 0;
 incrementOnOver(){
   this.onOver++; 
-}
+  }
 
-onLeave=0;
+onLeave  = 0;
 incrementOnLeave(){
   this.onLeave++; 
-}
+  }
+
+onDrag  = 0;
+incrementOnDrag(){
+  this.onDrag++; 
+  }
+
+onDragOver  = 0;
+incrementOnDragOver(){
+  this.onDragOver++; 
+  }
+
+//12. Binding Keyboard Events
+onKeyPress(){
+  console.log("key pressed");
+  }
+onKeyDown(){
+  console.log("key down event is triggered");
+  }
+//13. $event object
+eventObject(eventParameter:any){
+  console.log(eventParameter);
+  console.log(eventParameter.key);
+  //parameter is $event
+  }
+//14. Assignment (checking shift key status)
+isShift(event:any){
+  if(event.shiftKey && event.key ==='Y'){
+    console.log('Shift + Y is pressed');
+    }
+    console.log(event.key);
+  }
+//15. Two Way Binding ngModel
+staticInput:string = 'Static two-way binding';
+dynamicInput:string = '';
+
+//16. Two Way Binding ngModel
+inputValForButton:string='';
+dynamicVal:string='';
+show(){
+this.dynamicVal = this.inputValForButton;
+};
+buttonText:string = '';
 
 }
